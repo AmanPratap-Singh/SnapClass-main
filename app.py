7aa49cb8471cf1e263d6d738336110fbf881d994
Aman Pratap Singh <pratapsinghaman185@gmail.com>
2026-05-12 22:42:00 +0530

    Fix Quick Enrollment

--- app.py
+++ app.py
@@ -5,36 +5,45 @@ from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
 from src.components.dialog_auto_enroll import auto_enroll_dialog
 
+
 def main():
     st.set_page_config(
         page_title="SnapClass - Making Attendance faster using AI",
         page_icon="https://i.ibb.co/YTYGn5qV/logo.png"
     )
 
+    # session states
     if "login_type" not in st.session_state:
-        st.session_state["login_type"] = None
+        st.session_state.login_type = None
 
     if "show_auto_enroll" not in st.session_state:
         st.session_state.show_auto_enroll = False
 
-    match st.session_state["login_type"]:
+    # GET JOIN CODE
+    join_code = st.query_params.get("join_code")
+
+    # FORCE STUDENT SCREEN
+    if join_code and st.session_state.login_type != "student":
+        st.session_state.login_type = "student"
+
+    # SCREEN ROUTING
+    match st.session_state.login_type:
         case "teacher":
             teacher_screen()
+
         case "student":
             student_screen()
+
         case None:
             home_screen()
 
-    join_code = st.query_params.get('join_code')
+    # AUTO ENROLL AFTER LOGIN
+    if (
+        join_code
+        and st.session_state.get("is_logged_in")
+        and st.session_state.get("user_role") == "student"
+    ):
+        auto_enroll_dialog(join_code)
 
-    if join_code:
-    # force student screen
-        if st.session_state.login_type != 'student':
-            st.session_state.login_type = 'student'
-            st.rerun()
-
-        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
-            auto_enroll_dialog(join_code)
-    
 
 main()
